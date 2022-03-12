57d751ef13d59ee009edbccceba9c1e9316710e7
MrOscarbbx <misterioso1265@gmail.com>
2022-03-12 15:35:09 -0500

    Tarea1 Agregada

---
+++ Tarea1/Ejersicios/Concesionaria.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejersicios
+{
+    internal class Concesionaria
+    {
+        private Automovil[] automovils;
+        private int LIMITE;
+        private int numAutos = 0;
+
+        public Concesionaria(int limite)
+        {
+            this.LIMITE = limite;
+            automovils = new Automovil[LIMITE];
+        }
+
+
+        public void agregarAutomoviles(Automovil[] automoviles)
+        {
+            automovils = automoviles;
+        }
+        public void agregarLimite(int limite)
+        {
+            LIMITE = limite;
+        }
+        public void agregarNumAuto(int numAuto)
+        {
+            numAuto = numAuto;
+        } 
+
+        public void AgregarAutomovil(Automovil a)
+        {
+            if (!lleno()&& a!=null)
+            {
+                automovils[numAutos] = a;
+                numAutos++;
+            }
+            else
+            {
+                Console.WriteLine($"Cupo MAXIMO alcanzado {this.numAutos} o el Automovil no esxiste");
+
+            }
+        }
+
+        public void MostrarAuto (Automovil a)
+        {
+            Console.WriteLine($"{a.ToString()}");
+        }
+
+        public void EliminarAuto(Automovil a)
+        {
+            if (!(numAutos == 0))
+            {
+                int posEliminada = BuscarAuto(a);
+                if (posEliminada > 0)
+                {
+                     numAutos--;
+                    for (int posicion = posEliminada; posicion+1 < LIMITE; posicion++)
+                    {
+                        automovils[posicion] = automovils[posicion + 1];
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ese Auto no se encuentra la Cosecioraria");
+                }
+               
+            }
+        }
+
+        public int BuscarAuto(Automovil a)
+        {
+            if (!(numAutos == 0))
+            {
+                for (int posicion = 0; posicion <= numAutos; posicion++)
+                {
+                    if (automovils[posicion]==a)
+                    {
+                        return posicion;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        public void MostrarAutos()
+        {
+            for (int posicion = 0; posicion < numAutos; posicion++)
+            {
+                MostrarAuto(automovils[posicion]);
+            }
+        }
+        public void VaciarConcesionara()
+        {
+            numAutos = 0;
+        }
+        public Boolean lleno()
+        {
+            return numAutos == LIMITE;
+        }
+    }
+}
---
+++ Tarea1/Ejersicios/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using Ejersicios;
+
+Automovil auto = new Automovil(12345, "Toyota", "Supra", 120, 698000);
+Automovil auto1 = new Automovil(32275, "Nissan", "Supra", 42000, 62300);
+Automovil auto2 = new Automovil(21345, "Wolswaguen", "Supra", 14100,40000);
+Automovil auto3 = new Automovil(42445, "Chevy", "Supra", 42000, 11000);
+Automovil auto4 = new Automovil(62345, "Kia", "Supra", 1000, 111000);
+
+Concesionaria concesionaria = new Concesionaria(5);
+concesionaria.AgregarAutomovil(auto);
+concesionaria.AgregarAutomovil(auto1);
+concesionaria.AgregarAutomovil(auto2);
+concesionaria.AgregarAutomovil(auto3);
+concesionaria.MostrarAutos();
+Console.WriteLine("----------------------------------------------------");
+concesionaria.EliminarAuto(auto1);
+concesionaria.MostrarAutos();
+Console.WriteLine("----------------------------------------------------");
+concesionaria.VaciarConcesionara();
+concesionaria.MostrarAutos();
+
+
